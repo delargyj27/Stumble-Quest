537fd206361a34881541f31d08f98c06de8aedf8
JDelargy <delargyj27@gmail.com>
2024-02-21 15:51:46 -0500

    first commit

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+// Database connection
+$servername = "stumblequest.clu0m60664ab.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "dqPQpd4T2IOzHCjj6dUO";
+$database = "SQuest";
+
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+
+        if (password_verify($password, $row['password'])) {
+            $_SESSION["username"] = $username;
+            echo "Login successful";
+        } else {
+            echo "Invalid username or password";
+        }
+    } else {
+        echo "Invalid username or password";
+    }
+}
+
+$conn->close();
+?>
+
---
+++ register.php
@@ -0,0 +1,30 @@
+<?php
+
+// Database connection
+$servername = "stumblequest.clu0m60664ab.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "dqPQpd4T2IOzHCjj6dUO";
+$database = "SQuest";
+
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $user_type = $_POST["user_type"];
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, password, user_type) VALUES ('$username', '$hashed_password', '$user_type')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
