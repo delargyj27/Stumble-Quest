cd1d7c0e7e88b92269a3d70508e972168dda1691
jatalamantes <153138172+jatalamantes@users.noreply.github.com>
2024-02-27 14:41:09 -0500

    Update db_conn.php
    Added a connection check if successful message.

--- db_conn.php
+++ db_conn.php
@@ -5,8 +5,11 @@ $username = "admin";
 $password = "dqPQpd4T2IOzHCjj6dUO";
 $database = "SQuest";
 
-$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to the database!";
 }
 ?>
