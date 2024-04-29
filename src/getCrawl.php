6115d47e9d5e342846e6d40d77af7ea2feb0f128
Joe Kowalski <kowajw@farmingdale.edu>
2024-04-28 21:19:28 -0400

    added and tested query code getCrawl.php, added code to script crawl.html

--- src/getCrawl.php
+++ src/getCrawl.php
@@ -2,17 +2,32 @@
 
 include('db.php');
 
-//Select all columns from bars table
-$sql = "SELECT * FROM crawls WHERE crawl_id = ?";
-
-
 try {
+    //Select all columns from crawls table
+    $sql = "
+        SELECT * FROM
+            crawls 
+        WHERE crawl_id = ?
+    ";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$_GET["id"]]);
-    $data = $stmt->fetchAll(PDO::FETCH_ASSOC); //data resulting from that query
+    $crawlData = $stmt->fetchAll(PDO::FETCH_ASSOC); //data resulting from that query
+
+    //Select all columns from crawlbars table
+    $sql = "
+        SELECT * FROM
+            crawlbars 
+        WHERE crawl_id = ?
+    ";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_GET["id"]]);
+    $crawlBarsData = $stmt->fetchAll(PDO::FETCH_ASSOC); //data resulting from that query
 
     //Send data to front end 
-    echo json_encode($data[0], JSON_THROW_ON_ERROR);
+    echo json_encode([
+        "crawl" => $crawlData[0],
+        "crawlBars" => $crawlBarsData
+    ], JSON_THROW_ON_ERROR);
 } catch (Exception $e) {
     echo $e;
     die($e);
