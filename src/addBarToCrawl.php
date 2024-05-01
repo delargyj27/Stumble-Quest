465a818dc596265c28c96d7bc9f224dfdb34680a
Joe Kowalski <kowajw@farmingdale.edu>
2024-04-30 21:24:38 -0400

    Added error handling for addToCrawl

--- src/addBarToCrawl.php
+++ src/addBarToCrawl.php
@@ -1,38 +1,40 @@
 <?php
 
-include('db.php');
-
-// Get the request body
-$body = json_decode(file_get_contents("php://input"));
-
-//Select from bars table
-$sql = "
-    INSERT INTO 
-        crawlbars(crawl_id, barid, visitorder) 
-    VALUES
-        (
-            ?, 
-            ?, 
-            IFNULL(
-                (
-                    SELECT (visitorder + 1) 
-                    FROM crawlbars AS CB
-                    WHERE crawl_id = ?
-                    ORDER BY visitorder DESC
-                    LIMIT 1
-                ),
-                1
-            )
-        )
-";
-
 try {
+    include('db.php');
+
+    // Get the request body
+    $body = json_decode(file_get_contents("php://input"));
+
+    //Select from bars table
+    $sql = "
+        INSERT INTO 
+            crawlbars(crawl_id, barid, visitorder) 
+        VALUES
+            (
+                ?, 
+                ?, 
+                IFNULL(
+                    (
+                        SELECT (visitorder + 1) 
+                        FROM crawlbars AS CB
+                        WHERE crawl_id = ?
+                        ORDER BY visitorder DESC
+                        LIMIT 1
+                    ),
+                    1
+                )
+            )
+    ";
+
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$body->crawlId, $body->barId, $body->crawlId]);
 
     //Send data to front end - problem with this line here
     echo json_encode(["message" => "Bar added to crawl successfully!"], JSON_THROW_ON_ERROR);
 } catch (Exception $e) {
-    echo $e;
-    die($e);
+
+    echo json_encode([
+        "error" => $e->getMessage()
+    ], JSON_THROW_ON_ERROR);
 }
